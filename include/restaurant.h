aaed4bf2f135e9e647dd3bd6430e1feec34a5c5d
lebed <lebed@DESKTOP-BKIICKL>
2022-07-05 12:38:51 +0300

    28.4.3outMutex

--- include/restaurant.h
+++ include/restaurant.h
@@ -24,6 +24,7 @@ std::vector <Dishes> orders;
 std::vector <Dishes> readyDishes;
 std::mutex orderQueue;
 std::mutex readyDishesQueue;
+std::mutex outputQueue;
 
 std::string menuRestaurant(const Dishes dishes)
 {
@@ -48,7 +49,9 @@ void newOrder()
         orderQueue.lock();
         Dishes newDish = static_cast<Dishes> (rand() % AMOUNT_DISHES);
         orders.push_back(newDish);
+        outputQueue.lock();
         std::cout << "New order: " << menuRestaurant(newDish) << std::endl;
+        outputQueue.unlock();
         orderQueue.unlock();
     }
 }
@@ -72,7 +75,9 @@ void cooking()
         
         if (cook != NO_DISHES)
         {
+            outputQueue.lock();
             std::cout << "Cooking: " << menuRestaurant(orders[0]) << std::endl;
+            outputQueue.unlock();
             for (int i = 0; i < orders.size() - 1; ++i)
             {
                 orders[i] = orders[i + 1];
@@ -91,13 +96,17 @@ void cooking()
 
 void takeReadyDishes()
 {
-    for (int numberDelivery = 0; numberDelivery < 2; ++numberDelivery)
+    for (int numberDelivery = 0; numberDelivery < 10; ++numberDelivery)
     {
         std::this_thread::sleep_for(std::chrono::seconds(30));
         readyDishesQueue.lock();
+        outputQueue.lock();
         std::cout << "The courier received an order of " << readyDishes.size() << " dishes" << std::endl;
+        outputQueue.unlock();
         readyDishes.clear();
         readyDishesQueue.unlock();
+        outputQueue.lock();
         std::cout << numberDelivery + 1 <<"the order has been delivered " << std::endl;
+        outputQueue.unlock();
     }
 }
